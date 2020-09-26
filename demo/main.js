a972401ba3e79b2659bc25cf898927a5b79ab362
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-09-26 12:55:42 +0200

    add simple tracker

--- demo/main.js
+++ demo/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './app.vue'
+import './tracker'
 
 Vue.config.productionTip = false
 
---
+++ demo/tracker.js
@@ -0,0 +1,7 @@
+const key = '6oGbNAlraYnvOYxvJV54EL'
+const el = document.createElement('script')
+el.type = 'text/javascript'
+el.async = true
+el.src = 'https://cdn.howuku.com/js/howu.js'
+el.setAttribute('key', key)
+document.getElementsByTagName('head')[0].appendChild(el)
